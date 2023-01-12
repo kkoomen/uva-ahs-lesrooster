8a4764076bc2044a1d301df7eeb717bb1beb0c3c
Kim 金可明 <koomen@protonmail.com>
2023-01-12 18:13:43 +0100

    refactor: use csv.DictReader to improve readability

--- code/utils/data.py
+++ code/utils/data.py
@@ -10,16 +10,15 @@ def load_rooms() -> list[Room]:
     Load the lecture rooms data and create Room instances for each row.
     """
     rooms = []
-    with open(data_path('zalen.csv'), 'r') as file:
-        # Skip the header.
-        file.readline()
 
-        reader = csv.reader(file)
+    with open(data_path('zalen.csv'), 'r') as file:
+        reader = csv.DictReader(file)
         for row in reader:
-            room_id = row[0].strip()
-            capacity = int(row[1])
+            room_id = row['Zaalnummer'].strip()
+            capacity = int(row['Max. capaciteit'])
             rooms.append(Room(room_id, capacity))
         file.close()
+
     return rooms
 
 
@@ -28,19 +27,17 @@ def load_courses() -> list[Course]:
     Load the courses data and create Student instances for each row.
     """
     courses = []
-    with open(data_path('vakken.csv'), 'r') as file:
-        # Skip the header.
-        file.readline()
 
-        reader = csv.reader(file)
+    with open(data_path('vakken.csv'), 'r') as file:
+        reader = csv.DictReader(file)
         for row in reader:
-            name = row[0].strip()
-            lectures_amount = int(row[1])
-            seminars_amount = int(row[2])
-            seminar_capacity = int(row[3] or 0)
-            practicals_amount = int(row[4])
-            practical_capacity = int(row[5] or 0)
-            enrolment = int(row[6])
+            name = row['Vak'].strip()
+            lectures_amount = int(row['#Hoorcolleges'])
+            seminars_amount = int(row['#Werkcolleges'])
+            seminar_capacity = int(row['Max. stud. Werkcollege'] or 0)
+            practicals_amount = int(row['#Practica'])
+            practical_capacity = int(row['Max. stud. Practicum'] or 0)
+            enrolment = int(row['#Inschrijvingen'])
 
             course = Course(
                 name,
@@ -54,6 +51,7 @@ def load_courses() -> list[Course]:
 
             courses.append(course)
         file.close()
+
     return courses
 
 
@@ -62,10 +60,8 @@ def load_students() -> list[Student]:
     Load the students data and create Student instances for each row.
     """
     students = []
-    with open(data_path('studenten_en_vakken.csv'), 'r') as file:
-        # Skip the header.
-        file.readline()
 
+    with open(data_path('studenten_en_vakken.csv'), 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             last_name, first_name, student_number = row[:3]
@@ -73,4 +69,5 @@ def load_students() -> list[Student]:
             student = Student(first_name, last_name, student_number, enrolled_courses)
             students.append(student)
         file.close()
+
     return students
