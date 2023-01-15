efcc4c935558abd741b9bc59998da80c596af1d3
Kim 金可明 <koomen@protonmail.com>
2023-01-15 12:48:53 +0100

    feat(statistics): calculate avg, min and max malus scores

--- code/utils/statistics.py
+++ code/utils/statistics.py
@@ -11,11 +11,33 @@ def print_algorithm_average_statistics(algorithm: Algorithm,
     Runs a particular algorithm n-times and prints average statistics.
     """
     total_solutions = 0
+
     min_retries = None
     max_retries = None
     avg_retries = 0
+
+    min_malus_score = None
+    max_malus_score = None
+    avg_malus_score = 0
+
     for _ in range(iterations):
         found_solution, retries = algorithm.run()
+        malus_score = algorithm.timetable.calculate_malus_score()
+
+        if found_solution:
+            total_solutions += 1
+
+        # Calculate malus score statistics
+        avg_malus_score += malus_score
+
+        if min_malus_score is None or malus_score < min_malus_score:
+            min_malus_score = malus_score
+
+        if max_malus_score is None or malus_score > max_malus_score:
+            max_malus_score = malus_score
+
+
+        # Calculate retries statistics
         avg_retries += retries
 
         if min_retries is None or retries < min_retries:
@@ -24,15 +46,16 @@ def print_algorithm_average_statistics(algorithm: Algorithm,
         if max_retries is None or retries > max_retries:
             max_retries = retries
 
-        if found_solution:
-            total_solutions += 1
-
     avg_retries = int(avg_retries / iterations)
+    avg_malus_score = int(avg_malus_score / iterations)
 
     logger.info(f'Average info over {iterations} iterations for algorithm: {algorithm.__class__.__name__}')
     logger.info(f'\tMin. retries: {min_retries}')
     logger.info(f'\tMax. retries: {max_retries}')
     logger.info(f'\tAvg. retries: {avg_retries}')
+    logger.info(f'\tMin. malus score: {min_malus_score}')
+    logger.info(f'\tMax. malus score: {max_malus_score}')
+    logger.info(f'\tAvg malus score: {avg_malus_score}')
     logger.info(f'\tSolutions: {total_solutions}/{iterations}')
 
 
