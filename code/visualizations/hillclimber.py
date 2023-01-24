4599e19f502ccb0d8d03be9ccf8d9049e2aac282
Kim 金可明 <koomen@protonmail.com>
2023-01-24 14:52:38 +0100

    feat(visualizations/hillclimber): run algorithms parallel for faster result generation

--- code/visualizations/hillclimber.py
+++ code/visualizations/hillclimber.py
@@ -4,6 +4,7 @@ algorithms as their starting solution.
 """
 
 import matplotlib.pyplot as plt
+import concurrent.futures
 
 from code.algorithms.greedy import Greedy, GreedyLSD
 from code.algorithms.hillclimber import HillClimber
@@ -15,14 +16,24 @@ def plot_hillclimber_stats(iterations: int) -> None:
     Plot hill climber statistics using multiple algoritms in a single graph.
     """
     stats = {}
-    algs = [Randomizer, Greedy, GreedyLSD]
-    for class_ref in algs:
-        instance = class_ref()
-        hc = HillClimber(instance)
-        hc.run(iterations)
-        class_name = instance.__class__.__name__
-        stats[class_name] = hc.statistics
+    algorithms = [Randomizer, Greedy, GreedyLSD]
 
+    # Run the algorithms in parallel to speed up the generation.
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(algorithms)) as executor:
+        workers = []
+        for class_ref in algorithms:
+            def worker():
+                instance = class_ref()
+                hc = HillClimber(instance)
+                hc.run(iterations)
+                class_name = instance.__class__.__name__
+                stats[class_name] = hc.statistics
+            workers.append(executor.submit(worker))
+
+        # Wait for all workers to be completed.
+        concurrent.futures.wait(workers)
+
+    # Plot the gathered data.
     for (class_name, stats) in stats.items():
         plt.xlabel('iterations')
         plt.ylabel('malus points')
