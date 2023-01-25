a34ca1d767f1fef821be589e85f8b955cb9b2745
Kim 金可明 <koomen@protonmail.com>
2023-01-25 11:36:54 +0100

    feat(entities/room): allow is_largest to by passed through init

--- code/entities/room.py
+++ code/entities/room.py
@@ -4,10 +4,10 @@ class Room:
     an `Event` in order to schedule that `Event` in a particular room.
     """
 
-    def __init__(self, location_id: str, capacity: int) -> None:
+    def __init__(self, location_id: str, capacity: int, is_largest=False) -> None:
         self.location_id = location_id
         self.capacity = capacity
-        self.is_largest = False
+        self.is_largest = is_largest
 
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}(location_id:{self.location_id}, capacity:{self.capacity}, is_largest:{self.is_largest})'
