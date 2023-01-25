b7225a5890ecf2892a607962176414a3f6ee03b4
Kim 金可明 <koomen@protonmail.com>
2023-01-25 18:13:35 +0100

    test: use more readable names

--- test/entities/test_student.py
+++ test/entities/test_student.py
@@ -5,20 +5,20 @@ from code.entities.student import Student
 class TestStudent(TestCase):
 
     def test_init(self):
-        s = Student('John', 'Doe', '12345678', ['course1', 'course2'])
-        self.assertEqual(s.first_name, 'John')
-        self.assertEqual(s.last_name, 'Doe')
-        self.assertEqual(s.student_id, '12345678')
-        self.assertEqual(s.enrolled_courses, ['course1', 'course2'])
+        student = Student('John', 'Doe', '12345678', ['course1', 'course2'])
+        self.assertEqual(student.first_name, 'John')
+        self.assertEqual(student.last_name, 'Doe')
+        self.assertEqual(student.student_id, '12345678')
+        self.assertEqual(student.enrolled_courses, ['course1', 'course2'])
 
     def test_repr(self):
-        s = Student('John', 'Doe', '12345678', ['course1', 'course2'])
-        self.assertEqual(repr(s), "Student(first_name:John, last_name:Doe, student_id:12345678, enrolled_courses:['course1', 'course2'])")
+        student = Student('John', 'Doe', '12345678', ['course1', 'course2'])
+        self.assertEqual(repr(student), "Student(first_name:John, last_name:Doe, student_id:12345678, enrolled_courses:['course1', 'course2'])")
 
     def test_str(self):
-        s = Student('John', 'Doe', '12345678', ['course1', 'course2'])
-        self.assertEqual(str(s), 'John Doe')
+        student = Student('John', 'Doe', '12345678', ['course1', 'course2'])
+        self.assertEqual(str(student), 'John Doe')
 
     def test_get_full_name(self):
-        s = Student('John', 'Doe', '12345678', ['course1', 'course2'])
-        self.assertEqual(s.get_full_name(), 'John Doe')
+        student = Student('John', 'Doe', '12345678', ['course1', 'course2'])
+        self.assertEqual(student.get_full_name(), 'John Doe')
