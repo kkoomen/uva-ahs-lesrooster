8b57dd259f2194d26126b45056ea5efbc18ae1f6
Kim 金可明 <koomen@protonmail.com>
2023-01-25 11:37:08 +0100

    test(student): add more tests for student entity

--- test/entities/test_student.py
+++ test/entities/test_student.py
@@ -1,19 +1,24 @@
-#!/usr/bin/env python3
-
-
 from unittest import TestCase
 
 from code.entities.student import Student
 
 class TestStudent(TestCase):
 
-    def test_property_values(self):
-        first_name = 'John'
-        last_name = 'Doe'
-        student_id = '12345678'
-        enrolled_courses = ['course1', 'course2']
-        s = Student(first_name, last_name, student_id, enrolled_courses)
-        self.assertEqual(s.first_name, first_name)
-        self.assertEqual(s.last_name, last_name)
-        self.assertEqual(s.student_id, student_id)
-        self.assertEqual(s.enrolled_courses, enrolled_courses)
+    def test_init(self):
+        s = Student('John', 'Doe', '12345678', ['course1', 'course2'])
+        self.assertEqual(s.first_name, 'John')
+        self.assertEqual(s.last_name, 'Doe')
+        self.assertEqual(s.student_id, '12345678')
+        self.assertEqual(s.enrolled_courses, ['course1', 'course2'])
+
+    def test_repr(self):
+        s = Student('John', 'Doe', '12345678', ['course1', 'course2'])
+        self.assertEqual(repr(s), "Student(first_name:John, last_name:Doe, student_id:12345678, enrolled_courses:['course1', 'course2'])")
+
+    def test_str(self):
+        s = Student('John', 'Doe', '12345678', ['course1', 'course2'])
+        self.assertEqual(str(s), 'John Doe')
+
+    def test_get_full_name(self):
+        s = Student('John', 'Doe', '12345678', ['course1', 'course2'])
+        self.assertEqual(s.get_full_name(), 'John Doe')
